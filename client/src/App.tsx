7de20e20c79c819262a63ee09de67191ce174641
jami4you <jami4you@yahoo.com>
2025-09-06 12:45:11 -0400

    Created client server structure for project

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <h1 className="mt-2 text-3xl font-medium tracking-tight text-gray-950 dark:text-white">
+    Dashboard
+    </h1>
+  )
+}
+
+export default App
